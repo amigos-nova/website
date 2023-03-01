88392a220db445b0ac630b610148e6575acfaa78
Luis <luis.rodriguez@jhuapl.edu>
2023-03-01 13:21:05 -0500

    amigos site rough start template

---
+++ amigos-site/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="AMIGOS logo" src="./assets/logo.png" width="300">
+  <Home msg="AMIGOS"/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
